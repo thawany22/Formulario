03db6b29c397691da55bf3411de85fc2bd50e5be
Thawany22 <souzaathaay4@gmail.com>
2022-12-01 20:37:29 -0300

    tarefa realizada

---
+++ estilo.cs
@@ -0,0 +1,35 @@
+form{
+  
+font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+
+
+}
+
+
+form>{
+display: block;
+}
+
+
+body{
+
+    background: object value = url(imagem/etec-adolpho-berezin-part-ii.jpg) ;
+
+
+
+
+
+}
+
+h1{
+color: red;
+}
+label{
+color: red;
+}
+fieldset{
+    color: red;
+}
+p{
+    color: red;
+}
